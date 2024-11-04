094c33b30f79e7636d872693d0cb6c1829c6bae3
Hanka Celá <hana-cela@email.cz>
2024-11-04 20:15:46 +0100

    Vypracovani E-mailTelo1

--- script.js
+++ script.js
@@ -11,6 +11,6 @@ const fillBody = (body, name) => {
     goodbye(name);
 };
 
-// Vyzkoušejte funkce
+// Zavolání funkcí pro vyplnění e-mailu
 fillSubject('Žádost o zaměstnání');
 fillBody('Reaguji na Vaši nabídku práce na pozici frontend vývojář.', 'Pavel Ovesný');
